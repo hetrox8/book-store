3fdc0b763d7139de5fa88d502f188bcd793b46ec
suleiman gacheru <dretrevor8@gmail.com>
2023-08-19 03:13:19 +0300

    reset

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'; // Make sure to import React
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Book from './components/Books';
-import Navbar from './components/Navbar';
-import Categories from './components/Categories'; // Update component name to uppercase
+
+ // Update component name to uppercase
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Book />} />
-        <Route path='/categories' element={<Categories />} /> {/* Use uppercase component name */}
-      </Routes>
     </div>
   );
 }
