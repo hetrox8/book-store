0ec047c9d130515cb37ce9af2143cba75def0531
suleiman gacheru <dretrevor8@gmail.com>
2023-08-19 10:15:17 +0300

    solve eslint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,4 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
