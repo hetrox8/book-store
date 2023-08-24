b6c828710bdb2a5309d7c95516da0297260116f1
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-08-24 15:17:01 +0300

    Update AddingBook.js

--- src/components/AddingBook.js
+++ src/components/AddingBook.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import './Styles/Books.css';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/bookSlice';
+import './styles/Books.css';
 
-function AddBook() {
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      const newBook = {
+        item_id: Date.now(),
+        title,
+        author,
+        category: 'Some Category',
+      };
+      dispatch(addBook(newBook));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+
   return (
-    <div className="Book-F">
+    <div className="BookForm">
       <h3>Add New Book</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter Book Name"
+          value={title}
+          onChange={handleTitleChange}
         />
         <input
           type="text"
           placeholder="Enter Author Name"
+          value={author}
+          onChange={handleAuthorChange}
         />
-        <button className="Add-Book" type="submit"> Add Book </button>
+        <button className="AddBook" type="submit">
+          Add Book
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default AddBook;
