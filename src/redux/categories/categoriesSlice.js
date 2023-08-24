e15a8c886341c3db8d3e07a7b4c5c4db4d39ad00
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-08-24 15:03:14 +0300

    Update categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,29 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: '48 Laws of Power',
-      author: 'Robert Greene',
-      category: 'Personal Development',
-    },
-    {
-      id: 2,
-      title: 'Personal Development',
-      author: 'Jim Rohn',
-      category: 'Personal Development',
-    },
-  ],
+  status: [],
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    checkStatus: () => 'Under construction',
+    getStatus: (state, action) => {
+      state.status = !action.payload ? ['Under construction'] : [];
+    },
   },
 });
 
-export const { checkStatus } = categoriesSlice.actions;
+export const { getStatus } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
