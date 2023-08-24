4eab3bece74b81d4eb3dda1584576b1b6a697c20
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-08-24 15:13:34 +0300

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeBook } from '../redux/books/bookSlice';
 import './Styles/Books.css';
 
-function Book({ title, category, author }) {
+const Book = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books);
+
   return (
-    <div className="Single-B">
-      <h3>{title}</h3>
-      <p>{author}</p>
-      <p>{category}</p>
-      <button className="RemoveBook" type="button">Remove</button>
+    <div className="BookCard">
+      {books.map((item) => (
+        <div className="SingleBook" key={item.item_id}>
+          <h3>{item.title}</h3>
+          <p>{item.author}</p>
+          <p>{item.category}</p>
+          <button
+            className="RemoveBook"
+            type="button"
+            onClick={() => dispatch(removeBook(item.item_id))}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+
     </div>
   );
-}
-
-Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
 };
 
 export default Book;
