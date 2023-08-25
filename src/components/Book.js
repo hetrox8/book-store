09c6a906b9c1af7e91e844589533fbd935b2bb87
suleiman gacheru <dretrevor8@gmail.com>
2023-08-25 18:48:46 +0300

    fix elint on book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { DeleteBook, FetchData } from '../redux/book/bookSlice';
+import { fetchData, deleteBook } from '../redux/book/bookSlice';
+
 import './styles/Books.css';
 
 function Book() {
